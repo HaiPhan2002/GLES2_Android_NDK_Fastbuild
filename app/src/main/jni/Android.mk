5ef9d951a28a379b0b1b19b7130c64301bc9ca86
hai.phannhut <hai.phannhut@gameloft.com>
2018-07-21 13:21:53 +0700

    Update fbuild for armv8 & delete unused file

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := hello-GLES2
 LOCAL_CFLAGS := -Werror -std=c++11 -Wc++11-extensions -v
-LOCAL_SRC_FILES := gl_code.cpp
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../../../../android_fastbuild/gl_code.cpp
 
 LOCAL_C_INCLUDES := $(NDK_ROOT)/sysroot/usr/include
 
